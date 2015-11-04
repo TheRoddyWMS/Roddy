42392625cd93547f4918f6292750baaa2bd453ba
Michael Heinold <m.heinold@dkfz.de>
2015-11-04 16:15:34 +0100

    Change the libraries factory test class to be more clean and easy to read.

--- RoddyCore/test/de/dkfz/roddy/plugins/LibrariesFactoryTest.groovy
+++ RoddyCore/test/de/dkfz/roddy/plugins/LibrariesFactoryTest.groovy
@@ -2,15 +2,8 @@ package de.dkfz.roddy.plugins
 
 import de.dkfz.roddy.Roddy
 import de.dkfz.roddy.StringConstants
-import de.dkfz.roddy.client.cliclient.RoddyCLIClient
-import de.dkfz.roddy.core.Analysis
-import de.dkfz.roddy.core.ExecutionContext
-import de.dkfz.roddy.core.ExecutionContextLevel
-import de.dkfz.roddy.execution.io.ExecutionHelper
-import de.dkfz.roddy.tools.RoddyIOHelperMethods
-import de.dkfz.roddy.tools.Tuple2
-import org.junit.After
-import org.junit.Before
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
@@ -25,97 +18,101 @@ import java.lang.reflect.Method
 @groovy.transform.CompileStatic
 public class LibrariesFactoryTest {
 
-    private static Object testLock = new Object();
+//    private static Object testLock = new Object();
 
-    @Before
-    public void setUp() throws Exception {
+//    @Test
+//    /**
+//     * Test, if the plugin chain with "full" plugins is loading.
+//     */
+//    public void testLoadLibraryChainWithJarFile() {
+//        try {
+//            synchronized (testLock) {
+//                Roddy.resetMainStarted();
+//                Roddy.main(["testrun", "TestProjectForUnitTests@test", "stds", "--useRoddyVersion=current", "--disallowexit", "--configurationDirectories=" + RoddyIOHelperMethods.assembleLocalPath(Roddy.getApplicationDirectory(), "dist", "bin", "current", "testFiles").absolutePath] as String[]);
+//                assert (LibrariesFactory.getInstance().getLoadedLibrariesInfoList().size() == 3)
+//            }
+//        } finally {
+//            LibrariesFactory.initializeFactory(true);
+//        }
+//    }
+//
+//    @Test
+//    /**
+//     * Test, if the plugin chain with "mixed/empty" plugins is loading. Empty plugins are such, that have no jar file.
+//     */
+//    public void testLoadLibraryChainWithoutJarFile() {
+//        try {
+//            synchronized (testLock) {
+//                Roddy.resetMainStarted();
+//                Roddy.main(["testrun", "TestProjectForUnitTests@testWithoutJar", "stds", "--useRoddyVersion=current", "--disallowexit", "--configurationDirectories=" + RoddyIOHelperMethods.assembleLocalPath(Roddy.getApplicationDirectory(), "dist", "bin", "current", "testFiles").absolutePath] as String[]);
+//                assert (LibrariesFactory.getInstance().getLoadedLibrariesInfoList().size() == 4)
+//            }
+//        } finally {
+//            LibrariesFactory.initializeFactory(true);
+//        }
+//    }
 
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
-    @Test
     /**
-     * Test, if the plugin chain with "full" plugins is loading.
+     * Test data map for plugin chain loading mechanims
+     * The map contains various plugins, versions and dependencies to other plugins.
+     *  x.y.z / current (version)
+     *  -n (revision)
+     *  ,b (beta)
+     *  ,c (compatible)
+     *  ,bc (beta and compatible)
+     *  ,z (zip file (without e.g. buildinfo))
      */
-    public void testLoadLibraryChainWithJarFile() {
-        try {
-            synchronized (testLock) {
-                Roddy.resetMainStarted();
-                Roddy.main(["testrun", "TestProjectForUnitTests@test", "stds", "--useRoddyVersion=current", "--disallowexit", "--configurationDirectories=" + RoddyIOHelperMethods.assembleLocalPath(Roddy.getApplicationDirectory(), "dist", "bin", "current", "testFiles").absolutePath] as String[]);
-                assert (LibrariesFactory.getInstance().getLoadedLibrariesInfoList().size() == 3)
-            }
-        } finally {
-            LibrariesFactory.initializeFactory(true);
-        }
-    }
+    private static final LinkedHashMap<String, LinkedHashMap<String, List<String>>> mapWithTestPlugins = [
+            A: ["1.0.1"  : [],
+                "1.0.24" : ["PluginBase:1.0.24", "DefaultPlugin:1.0.28"],
+                "current": ["PluginBase:current", "DefaultPlugin:current"]
+            ],
+            B: ["0.9.0"    : ["A:1.0.1"],
+                "1.0.1"    : ["A:1.0.24"],
+                "1.0.1-r"  : [],   // Is not valid and will be filtered
+                "1.0.2"    : ["A:1.0.24"],
+                "1.0.2-1"  : ["A:1.0.24"],
+                "1.0.2-2,b": ["A:1.0.24"],
+                "1.0.3,c"  : ["A:current"]
+            ],
+            C: ["0.9.0"    : ["B:0.9.0"],
+                "1.0.0,z"  : [], // Also test, if zipped plugins can be recognized. There should come a warning and they will get filtered.
+                "1.0.0"    : [], // Unzipped version of the prior zip file
+                "1.0.1"    : ["B:1.0.1"],
+                "1.0.2,c"  : ["B:1.0.2-1"],
+                "1.0.3"    : ["B:1.0.3"],
+                "current,c": ["B:1.0.3"]
+            ],
+            D: ["0.9.0"  : ["C:0.9.0"],
+                "1.0.1"  : ["C:1.0.1"],
+                "1.0.2"  : ["C:1.0.2", "B:1.0.1"],
+                "1.0.2-1": ["C:1.0.2"],
+                "1.0.3"  : ["C:1.0.3", "B:1.0.2"],
+                "1.0.4,z": [],
+                "current": ["C:current"]
+            ],
+    ]
 
-    @Test
     /**
-     * Test, if the plugin chain with "mixed/empty" plugins is loading. Empty plugins are such, that have no jar file.
+     * Keeps all available plugins which were created with the help of the mapWithTestPlugins map
      */
-    public void testLoadLibraryChainWithoutJarFile() {
-        try {
-            synchronized (testLock) {
-                Roddy.resetMainStarted();
-                Roddy.main(["testrun", "TestProjectForUnitTests@testWithoutJar", "stds", "--useRoddyVersion=current", "--disallowexit", "--configurationDirectories=" + RoddyIOHelperMethods.assembleLocalPath(Roddy.getApplicationDirectory(), "dist", "bin", "current", "testFiles").absolutePath] as String[]);
-                assert (LibrariesFactory.getInstance().getLoadedLibrariesInfoList().size() == 4)
-            }
-        } finally {
-            LibrariesFactory.initializeFactory(true);
-        }
+    private static Map<String, Map<String, PluginInfo>> mapOfAvailablePlugins
+
+    public static TemporaryFolder pluginsBaseDir = new TemporaryFolder();
+
+    @AfterClass
+    public static void tearDownClass() {
+        pluginsBaseDir.delete();
     }
 
-    @Rule
-    public TemporaryFolder pluginsBaseDir = new TemporaryFolder();
+    @BeforeClass
+    public static void setupTestDataForPluginQueueTests() {
 
-    @Test
-    public void testLoadPluginsFromDirectories() {
-        // Build up test data
-        // First, create a map of plugins and versions
-        //  x.y.z / current (version)
-        //  -n (revision)
-        //  ,b (beta)
-        //  ,c (compatible)
-        //  ,bc (beta and compatible)
-        //  ,z (zip file (without e.g. buildinfo))
-        LinkedHashMap<String, LinkedHashMap<String, List<String>>> testPluginsList = [
-                A: ["1.0.1"  : [],
-                    "1.0.24" : ["PluginBase:1.0.24", "DefaultPlugin:1.0.28"],
-                    "current": ["PluginBase:current", "DefaultPlugin:current"]
-                ],
-                B: ["0.9.0"    : ["A:1.0.1"],
-                    "1.0.1"    : ["A:1.0.24"],
-                    "1.0.1-r"  : [],   // Is not valid and will be filtered
-                    "1.0.2"    : ["A:1.0.24"],
-                    "1.0.2-1"  : ["A:1.0.24"],
-                    "1.0.2-2,b": ["A:1.0.24"],
-                    "1.0.3,c"  : ["A:current"]
-                ],
-                C: ["0.9.0"    : ["B:0.9.0"],
-                    "1.0.0,z"  : [], //Also test, if zipped plugins can be recognized. There should come a warning and they will get filtered.
-                    "1.0.0"    : [], //Also test, if zipped plugins can be recognized. There should come a warning and they will get filtered.
-                    "1.0.1"    : ["B:1.0.1"],
-                    "1.0.2,c"  : ["B:1.0.2-1"],
-                    "1.0.3"    : ["B:1.0.3"],
-                    "current,c": ["B:1.0.3"]
-                ],
-                D: ["0.9.0"  : ["C:0.9.0"],
-                    "1.0.1"  : ["C:1.0.1"],
-                    "1.0.2"  : ["C:1.0.2", "B:1.0.1"],
-                    "1.0.2-1": ["C:1.0.2"],
-                    "1.0.3"  : ["C:1.0.3", "B:1.0.2"],
-                    "1.0.4,z": [],
-                    "current": ["C:current"]
-                ],
-        ]
+        pluginsBaseDir.create();
 
         // Create the folder and file structure so that the loadPluginsFromDirectories method will work.
-        for (String plugin : testPluginsList.keySet()) {
-            Map<String, List<String>> pluginInfoMap = testPluginsList[plugin];
+        for (String plugin : mapWithTestPlugins.keySet()) {
+            Map<String, List<String>> pluginInfoMap = mapWithTestPlugins[plugin];
             List<String> listOfVersions = pluginInfoMap.keySet() as List<String>;
             for (int i = 0; i < listOfVersions.size(); i++) {
                 String version = listOfVersions[i];
@@ -160,28 +157,25 @@ public class LibrariesFactoryTest {
         loadMapOfAvailablePlugins.setAccessible(true);
 
         // Invoke the method and check the results.
-        Map<String, Map<String, PluginInfo>> res = loadMapOfAvailablePlugins.invoke(null, pluginDirectories) as Map<String, Map<String, PluginInfo>>;
+        mapOfAvailablePlugins = loadMapOfAvailablePlugins.invoke(null, pluginDirectories) as Map<String, Map<String, PluginInfo>>;
 
         // Check, if all plugins were recognized and if the version count matches.
-        assert res.size() >= testPluginsList.size(); // Take the additional plugins into account
-        assert res["PluginBase"]["current-0"] == null && res["PluginBase"]["current"] != null
-        assert testPluginsList["A"].size() == res["A"].size();
-        assert testPluginsList["B"].size() == res["B"].size() + 1; // Lacks one filtered entry.
-        assert testPluginsList["C"].size() == res["C"].size() + 1; // Lacks one filtered zip entry.
-        assert testPluginsList["D"].size() == res["D"].size() + 1; // Lacks one filtered zip entry.
+        assert mapOfAvailablePlugins.size() >= mapWithTestPlugins.size(); // Take the additional plugins into account
+        assert mapOfAvailablePlugins["PluginBase"]["current-0"] == null && mapOfAvailablePlugins["PluginBase"]["current"] != null
+        assert mapWithTestPlugins["A"].size() == mapOfAvailablePlugins["A"].size();
+        assert mapWithTestPlugins["B"].size() == mapOfAvailablePlugins["B"].size() + 1; // Lacks one filtered entry.
+        assert mapWithTestPlugins["C"].size() == mapOfAvailablePlugins["C"].size() + 1; // Lacks one filtered zip entry.
+        assert mapWithTestPlugins["D"].size() == mapOfAvailablePlugins["D"].size() + 1; // Lacks one filtered zip entry.
 
-        assert res["B"]["1.0.2-1"]?.previousInChain == res["B"]["1.0.2-0"] && res["B"]["1.0.2-1"]?.previousInChainConnectionType == PluginInfo.PluginInfoConnection.REVISION
-        assert res["B"]["1.0.3-0"]?.previousInChain == res["B"]["1.0.2-2"] && res["B"]["1.0.3-0"]?.previousInChainConnectionType == PluginInfo.PluginInfoConnection.EXTENSION
+        assert mapOfAvailablePlugins["B"]["1.0.2-1"]?.previousInChain == mapOfAvailablePlugins["B"]["1.0.2-0"] && mapOfAvailablePlugins["B"]["1.0.2-1"]?.previousInChainConnectionType == PluginInfo.PluginInfoConnection.REVISION
+        assert mapOfAvailablePlugins["B"]["1.0.3-0"]?.previousInChain == mapOfAvailablePlugins["B"]["1.0.2-2"] && mapOfAvailablePlugins["B"]["1.0.3-0"]?.previousInChainConnectionType == PluginInfo.PluginInfoConnection.EXTENSION
+    }
 
+    @Test
+    public void testBuildupValidPluginQueue() {
         // Now test several plugin chains for consistency and expected results.
-        Map<String, PluginInfo> pluginQueueWODefaultLibs = LibrariesFactory.buildupPluginQueue(res, ["D:0.9.0"] as String[]);
-        Map<String, PluginInfo> pluginQueueFaulty = LibrariesFactory.buildupPluginQueue(res, ["D:1.0.2"] as String[]);
-        Map<String, PluginInfo> pluginQueueOK = LibrariesFactory.buildupPluginQueue(res, ["D:1.0.2-1"] as String[]);
-        Map<String, PluginInfo> pluginQueueCompatible = LibrariesFactory.buildupPluginQueue(res, ["D:1.0.3"] as String[]);
-        Map<String, PluginInfo> pluginQueueFixatedEntriesFaulty = LibrariesFactory.buildupPluginQueue(res, ["D:1.0.3", "C:1.0.3", "B:1.0.2-2"] as String[]);
-        Map<String, PluginInfo> pluginQueueFixatedEntriesOK = LibrariesFactory.buildupPluginQueue(res, ["D:1.0.2-1", "C:1.0.2", "B:1.0.2-1"] as String[]);
-
-        //Finally validate several plugin chains and look if all to-be-loaded versions are properly found.
+        // Validate if all to-be-loaded versions are properly found.
+        Map<String, PluginInfo> pluginQueueOK = LibrariesFactory.buildupPluginQueue(mapOfAvailablePlugins, ["D:1.0.2-1"] as String[]);
         assert pluginQueueOK != null;
         assert pluginQueueOK["D"].prodVersion == "1.0.2-1" &&
                 pluginQueueOK["C"].prodVersion == "1.0.2-0" &&
@@ -189,7 +183,19 @@ public class LibrariesFactoryTest {
                 pluginQueueOK["A"].prodVersion == "current" &&
                 pluginQueueOK["PluginBase"].prodVersion == "current" &&
                 pluginQueueOK["DefaultPlugin"].prodVersion == "current";
+
+    }
+
+    @Test
+    public void testBuildupInvalidPluginQueue() {
+        Map<String, PluginInfo> pluginQueueFaulty = LibrariesFactory.buildupPluginQueue(mapOfAvailablePlugins, ["D:1.0.2"] as String[]);
         assert pluginQueueFaulty == null;
+
+    }
+
+    @Test
+    public void testBuildupPluginQueueContainingCompatibleEntries() {
+        Map<String, PluginInfo> pluginQueueCompatible = LibrariesFactory.buildupPluginQueue(mapOfAvailablePlugins, ["D:1.0.3"] as String[]);
         assert pluginQueueCompatible != null;
         assert pluginQueueCompatible["D"].prodVersion == "1.0.3-0" &&
                 pluginQueueCompatible["C"].prodVersion == "current" &&
@@ -197,7 +203,19 @@ public class LibrariesFactoryTest {
                 pluginQueueCompatible["A"].prodVersion == "current" &&
                 pluginQueueCompatible["PluginBase"].prodVersion == "current" &&
                 pluginQueueCompatible["DefaultPlugin"].prodVersion == "current";
+
+    }
+
+    @Test
+    public void testBuildupInvalidPluginQueueWithFixatedEntries() {
+        Map<String, PluginInfo> pluginQueueFixatedEntriesFaulty = LibrariesFactory.buildupPluginQueue(mapOfAvailablePlugins, ["D:1.0.3", "C:1.0.3", "B:1.0.2-2"] as String[]);
         assert pluginQueueFixatedEntriesFaulty == null;
+
+    }
+
+    @Test
+    public void testBuildupValidPluginQueueWithFixatedEntries() {
+        Map<String, PluginInfo> pluginQueueFixatedEntriesOK = LibrariesFactory.buildupPluginQueue(mapOfAvailablePlugins, ["D:1.0.2-1", "C:1.0.2", "B:1.0.2-1"] as String[]);
         assert pluginQueueFixatedEntriesOK != null;
         assert pluginQueueFixatedEntriesOK["D"].prodVersion == "1.0.2-1" &&
                 pluginQueueFixatedEntriesOK["C"].prodVersion == "1.0.2-0" &&
@@ -205,9 +223,15 @@ public class LibrariesFactoryTest {
                 pluginQueueFixatedEntriesOK["A"].prodVersion == "1.0.24-0" &&
                 pluginQueueFixatedEntriesOK["PluginBase"].prodVersion == "1.0.24-0" &&
                 pluginQueueFixatedEntriesOK["DefaultPlugin"].prodVersion == "1.0.28-0";
+
+    }
+
+    @Test
+    public void testBuildupValidPluginQueueWithMissingDefaultLibraryEntries() {
+        Map<String, PluginInfo> pluginQueueWODefaultLibs = LibrariesFactory.buildupPluginQueue(mapOfAvailablePlugins, ["D:0.9.0"] as String[]);
         assert pluginQueueWODefaultLibs != null;
         assert pluginQueueWODefaultLibs["PluginBase"].prodVersion == "current" &&
                 pluginQueueWODefaultLibs["DefaultPlugin"].prodVersion == "current";
-        println("Please ignore severe errors above, test was successful and the displayed errors are necessary.")
     }
+
 }
