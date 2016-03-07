df98230c447ec02424d0d15cace52618a9d3d2fc
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-03-07 11:05:27 +0100

    PluginBase Release 1.0.28 (changed buildinfo.txt for API compatibility options).

--- dist/plugins/PluginBase/src/de/dkfz/roddy/plugins/BasePlugin.java
+++ dist/plugins/PluginBase/src/de/dkfz/roddy/plugins/BasePlugin.java
@@ -5,8 +5,8 @@ package de.dkfz.roddy.plugins;
  */
 public abstract class BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.0.27";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Tue Jan 19 10:49:51 CET 2016";
+    public static final String CURRENT_VERSION_STRING = "1.0.28";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Mon Mar 07 10:58:54 CET 2016";
 
     public abstract String getVersionInfo();
 }
