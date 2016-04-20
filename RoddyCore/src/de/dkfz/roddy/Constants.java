aed6578c1e2629d6f54c433971dd3d83f0f076a2
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-04-20 11:50:01 +0200

    Bugfix for omitted plugin directories: Skip over plugins with buildinfo.txt that does not have dependson=... in first line. Minor fix in roddyCall.sh file.
    Test Plan: none
    
    Reviewers: heinold
    
    Differential Revision: https://tbi-cb14.inet.dkfz-heidelberg.de/D191

--- RoddyCore/src/de/dkfz/roddy/Constants.java
+++ RoddyCore/src/de/dkfz/roddy/Constants.java
@@ -10,8 +10,8 @@ public class Constants {
     // Application constants
     /////////////////////////
 
-    public static final String APP_CURRENT_VERSION_STRING = "2.2.72";
-    public static final String APP_CURRENT_VERSION_BUILD_DATE = "Mon Apr 18 13:02:09 CEST 2016";
+    public static final String APP_CURRENT_VERSION_STRING = "2.2.73";
+    public static final String APP_CURRENT_VERSION_BUILD_DATE = "Mon Apr 18 13:47:00 CEST 2016";
     public static final String APP_PROPERTY_COMMAND_FACTORY_CLASS = "commandFactoryClass";
     public static final String APP_PROPERTY_FILESYSTEM_INFO_PROVIDER_CLASS = "fileSystemInfoProviderClass";
     public static final String APP_PROPERTY_EXECUTION_SERVICE_CLASS = "executionServiceClass";
