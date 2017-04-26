73823a09901d437eec46ed3f3365fa38620c8d76
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2017-04-26 10:24:12 +0200

    Correct cyclic dependency detection.
    Add CollectionHelperMethods class which offers new methods:
      intersect(Coll, Coll) which gives you the intersection of
        [1,2,3].intersect([3,4,5]) == [3]
      intersects, which is true, if two colletions intersect
      intersectList which returns a List of intersected values
    
    Correct the cyclic check. Introduce a blacklist, which is passed down to
    every call of CValue.toString. Everytime, the name of the current value
    is appended. If the name of the current value is in the blacklist, an
    error is thrown.

--- RoddyCore/src/de/dkfz/roddy/config/ConfigurationValue.java
+++ RoddyCore/src/de/dkfz/roddy/config/ConfigurationValue.java
@@ -11,6 +11,7 @@ import de.dkfz.roddy.core.Analysis;
 import de.dkfz.roddy.core.DataSet;
 import de.dkfz.roddy.core.ExecutionContext;
 import de.dkfz.roddy.execution.io.fs.FileSystemAccessProvider;
+import de.dkfz.roddy.tools.CollectionHelperMethods;
 import de.dkfz.roddy.tools.RoddyConversionHelperMethods;
 
 import java.io.File;
@@ -269,17 +270,25 @@ public class ConfigurationValue implements RecursiveOverridableMapContainer.Iden
 
     @Override
     public String toString() {
+        return toString(new LinkedList<>());
+    }
+
+    public String toString(List<String> blackList) {
         String temp = value;
         if (configuration != null) {
             List<String> valueIDs = getIDsForParentValues();
+            if (CollectionHelperMethods.intersects(blackList, valueIDs)) {
+                RuntimeException exc = new RuntimeException("Cyclic dependency found for cvalue '" + this.id + "' in file " + (configuration.informationalConfigurationContent != null ? configuration.informationalConfigurationContent.file : configuration.getID()));
+                configuration.addLoadError(new ConfigurationLoadError(configuration, "cValues", exc.getMessage(), exc));
+                throw exc;
+            }
+
             for (String vName : valueIDs) {
-                if (temp.contains("${" + vName) || temp.contains("${cvalue,name=\"" + vName)){
-                    RuntimeException exc = new RuntimeException("Cyclic dependency found for cvalue '" + vName + "'");
-                    configuration.addLoadError(new ConfigurationLoadError(configuration, "cValues", "Cyclic dependency found for cvalue '" + vName + "'", exc));
-                    throw exc;
+                if (configuration.getConfigurationValues().hasValue(vName)) {
+                    List<String> subBlackList = new LinkedList<>(blackList);
+                    subBlackList.add(vName);
+                    temp = temp.replace("${" + vName + '}', configuration.getConfigurationValues().get(vName).toString(subBlackList));
                 }
-                if (configuration.getConfigurationValues().hasValue(vName))
-                    temp = temp.replace("${" + vName + '}', configuration.getConfigurationValues().get(vName).toString());
             }
         }
         return temp;
