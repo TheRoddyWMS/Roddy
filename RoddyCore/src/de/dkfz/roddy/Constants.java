8f9cf8a816fb7ccd1e37def304245eaf8d93e80e
Michael Heinold <m.heinold@dkfz-heidelberg.de>
2017-06-29 09:39:34 +0200

    2.3.187b Release

--- RoddyCore/src/de/dkfz/roddy/Constants.java
+++ RoddyCore/src/de/dkfz/roddy/Constants.java
@@ -16,8 +16,8 @@ public class Constants {
     // Application constants
     /////////////////////////
 
-    public static final String APP_CURRENT_VERSION_STRING = "2.3.186";
-    public static final String APP_CURRENT_VERSION_BUILD_DATE = "Fri Jun 16 10:18:04 CEST 2017";
+    public static final String APP_CURRENT_VERSION_STRING = "2.3.187";
+    public static final String APP_CURRENT_VERSION_BUILD_DATE = "Wed Jun 28 13:48:34 CEST 2017";
     public static final String APP_PROPERTY_COMMAND_FACTORY_CLASS = "jobManagerClass";
     public static final String APP_PROPERTY_FILESYSTEM_ACCESS_MANAGER_CLASS = "fileSystemAccessManagerClass";
     public static final String APP_PROPERTY_EXECUTION_SERVICE_CLASS = "executionServiceClass";
