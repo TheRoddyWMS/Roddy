9e0b073f327d244cafb8bf1246cb3cc35a2c2ed9
dankwart-de <m.heinold@dkfz-heidelberg.de>
2018-07-06 13:22:16 +0200

    Fix failing test, remove obsolete test
    Change RuntimeTools so that:
    - getRoddyRuntimeVersion will check for a regex with number.number
    - getJavaRuntimeVersion is removed

---
+++ RoddyCore/test/src/de/dkfz/roddy/tools/RuntimeToolsTest.groovy
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 eilslabs.
+ *
+ * Distributed under the MIT License (license terms are at https://www.github.com/eilslabs/Roddy/LICENSE.txt).
+ */
+
+package de.dkfz.roddy.tools
+
+import de.dkfz.roddy.Roddy
+import groovy.transform.CompileStatic
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+
+/**
+ * This test class does runtime environment specific tests. It is not very well written and uses some hardcoded elements.
+ * However, the dependency to the runtime system makes it necessary to somehow hardcode things :(... Don't currently know, how to make it better.
+ * Created by heinold on 20.06.16.
+ */
+@CompileStatic
+class RuntimeToolsTest {
+    @Test
+    void getRoddyRuntimeVersion() throws Exception {
+        RuntimeTools.getRoddyRuntimeVersion() ==~ "[0-9]*.[0-9]*"
+    }
+
+    @Test
+    void testGetBuildinfoFile() {
+        String buildinfoTextFile = RuntimeTools.getBuildinfoFile().getAbsolutePath()
+        String estimatedBuildInfoTextFile = RoddyIOHelperMethods.assembleLocalPath(Roddy.getApplicationDirectory(), "dist", "bin", "develop", "buildinfo.txt").getAbsolutePath()
+        assertEquals(estimatedBuildInfoTextFile, buildinfoTextFile)
+    }
+
+    @Test
+    void testGetDevelopmentDistFolder() {
+        assertEquals(
+                RoddyIOHelperMethods.assembleLocalPath(Roddy.getApplicationDirectory(), "dist", "bin", "develop").getAbsolutePath(),
+                RuntimeTools.getDevelopmentDistFolder().getAbsolutePath())
+    }
+
+}
