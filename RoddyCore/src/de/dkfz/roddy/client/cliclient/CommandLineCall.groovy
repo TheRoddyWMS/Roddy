4153fdc92df898294803eedd064a4bcaa73d9e08
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2018-12-13 18:16:04 +0100

    Removed most codenarc warnings.

--- RoddyCore/src/de/dkfz/roddy/client/cliclient/CommandLineCall.groovy
+++ RoddyCore/src/de/dkfz/roddy/client/cliclient/CommandLineCall.groovy
@@ -21,53 +21,53 @@ import static de.dkfz.roddy.client.RoddyStartupModes.help
  * Options and parameters are extracted and put into an object of this class.
  */
 @groovy.transform.CompileStatic
-public class CommandLineCall {
-    private static LoggerWrapper logger = LoggerWrapper.getLogger(RoddyCLIClient.class.getSimpleName());
-    public final RoddyStartupModes startupMode;
-    private final List<String> arguments;
-    private final List<String> parameters;
-    private Map<RoddyStartupOptions, List<String>> optionsMap;
+class CommandLineCall {
+    private static LoggerWrapper logger = LoggerWrapper.getLogger(RoddyCLIClient.class.getSimpleName())
+    public final RoddyStartupModes startupMode
+    private final List<String> arguments
+    private final List<String> parameters
+    private Map<RoddyStartupOptions, List<String>> optionsMap
     final boolean malformed
 
     CommandLineCall(List<String> args) {
-        parameters = [];
-        optionsMap = [:];
+        parameters = []
+        optionsMap = [:]
         if (!args.size() == 0)
-            args = [RoddyStartupModes.help.toString()];
-        this.arguments = args;
+            args = [help.toString()]
+        this.arguments = args
 
-        List<String> parameters = args.findAll { String arg -> !arg.startsWith("--") } as ArrayList<String>;
-        Collection<String> options = args.findAll { String arg -> arg.startsWith("--") };
+        List<String> parameters = args.findAll { String arg -> !arg.startsWith('--') } as ArrayList<String>
+        Collection<String> options = args.findAll { String arg -> arg.startsWith('--') }
 
         // Try to extract the startup mode. If it not know, display the help message.
         try {
-            startupMode = parameters ? parameters[0] as RoddyStartupModes : help;
+            startupMode = parameters ? parameters[0] as RoddyStartupModes : help
         } catch (Exception ex) {
-            logger.postAlwaysInfo("The startupmode " + parameters[0] + " is not known.");
-            this.startupMode = help;
-            return;
+            logger.postAlwaysInfo("The startupmode " + parameters[0] + " is not known.")
+            this.startupMode = help
+            return
         }
 
         //Parse options
         List<String> errors = []
-        Map<RoddyStartupOptions, List<String>> parsedOptions = [:];
+        Map<RoddyStartupOptions, List<String>> parsedOptions = [:]
         for (String optArg in options) {
-            String[] split = optArg.split(StringConstants.SPLIT_EQUALS);
+            String[] split = optArg.split(StringConstants.SPLIT_EQUALS)
             try {
-                RoddyStartupOptions option = split[0][2..-1] as RoddyStartupOptions;
+                RoddyStartupOptions option = split[0][2..-1] as RoddyStartupOptions
                 //TODO This needs to be reworked because:
                 //i.e. --cvalues=test:abc,test2:(e,e,f),test3("A string, with a comma") will seriously make problems!
                 //Leave it for now, but come back to it if it is necessary.
-                List<String> values = option.acceptsParameters ? split[1].split(StringConstants.SPLIT_COMMA)?.toList() : null;
-                parsedOptions[option] = values;
+                List<String> values = option.acceptsParameters ? split[1].split(SPLIT_COMMA)?.toList() : null
+                parsedOptions[option] = values
                 if (option.acceptsParameters)
                     if (values)
-                        parsedOptions[option] = values;
+                        parsedOptions[option] = values
                     else {
                         errors << "The option " + option + " is malformed!"
                     }
             } catch (Exception ex) {
-                errors << "The option with " + optArg + " is malformed!";
+                errors << "The option with " + optArg + " is malformed!"
             }
         }
 
@@ -77,44 +77,44 @@ public class CommandLineCall {
 
         // Store all parameters and remove the startup mode.
         if (parameters.size() > 1)
-            this.parameters += parameters[1..-1];
-        this.optionsMap.putAll(parsedOptions);
+            this.parameters += parameters[1..-1]
+        this.optionsMap.putAll(parsedOptions)
     }
 
     List<String> getParameters() {
-        return new LinkedList(parameters);
+        new LinkedList(parameters)
     }
 
     String getAnalysisID() {
-        return parameters[0];
+        parameters[0]
     }
 
     List<String> getDatasetSpecifications() {
-        return Arrays.asList(parameters[1].split(SPLIT_COMMA));
+        Arrays.asList(parameters[1].split(SPLIT_COMMA))
     }
 
-    public boolean hasParameters() {
-        return parameters.size() > 0;
+    boolean hasParameters() {
+        parameters.size() > 0
     }
 
-    public boolean isOptionSet(RoddyStartupOptions option) {
-        return optionsMap.containsKey(option);
+    boolean isOptionSet(RoddyStartupOptions option) {
+        optionsMap.containsKey(option)
     }
 
-    public List<RoddyStartupOptions> getOptionList() {
-        return optionsMap.keySet().asList();
+    List<RoddyStartupOptions> getOptionList() {
+        optionsMap.keySet().asList()
     }
 
-    public String getOptionValue(RoddyStartupOptions option) {
-        return optionsMap.get(option)?.first();
+    String getOptionValue(RoddyStartupOptions option) {
+        optionsMap.get(option)?.first()
     }
 
-    public List<String> getOptionList(RoddyStartupOptions option) {
-        return optionsMap.get(option);
+    List<String> getOptionList(RoddyStartupOptions option) {
+        optionsMap.get(option)
     }
 
-    public List<String> getArguments() {
-        return arguments;
+    List<String> getArguments() {
+        arguments
     }
 
     /**
@@ -122,16 +122,17 @@ public class CommandLineCall {
      * [ a:a string, b:another string, c:123, ... ]
      * @return
      */
-    public List<ConfigurationValue> getSetConfigurationValues() {
-        def externalConfigurationValues = optionsMap.get(RoddyStartupOptions.cvalues, []);
+    List<ConfigurationValue> getSetConfigurationValues() {
+        def externalConfigurationValues = optionsMap.get(RoddyStartupOptions.cvalues, [])
         def configurationValues = []
         for (eVal in externalConfigurationValues) {
-            String[] splitIDValue = eVal.split(StringConstants.SPLIT_COLON);
+            String[] splitIDValue = eVal.split(StringConstants.SPLIT_COLON)
             //TODO Put in a better error checking when converting the split string to a configuration value.
             //Remark, if a value contains : / colons, it will not work!
-            String cvalueId = splitIDValue[0];
-            String value = (splitIDValue.size() >= 2 ? splitIDValue[1] : ""); // Surround value with single quotes '' to prevent string interpretation for e.g. execution in bash
-            String type = splitIDValue.size() >= 3 ? splitIDValue[2] : null; // If null is set, type is guessed.
+            String cvalueId = splitIDValue[0]
+            // Surround value with single quotes '' to prevent string interpretation for e.g. execution in bash
+            String value = (splitIDValue.size() >= 2 ? splitIDValue[1] : '')
+            String type = splitIDValue.size() >= 3 ? splitIDValue[2] : null // If null is set, type is guessed.
 
             configurationValues << new ConfigurationValue(cvalueId, value, type)
         }
