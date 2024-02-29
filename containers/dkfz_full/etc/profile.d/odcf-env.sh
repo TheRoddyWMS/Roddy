b21e0fd20b68c8472b489f4e155054e3d7c8779a
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2024-03-01 00:00:00 +0100

    Removed the containers/dkfz_full directory. The full-blown container is not yet ready.
    Signed-off-by: Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
    
    GitPrivacy: rH05vw/BZ8mephvAtQMM1OgME3zWCCWivmvYuIiCJk7oMmuqGVTKEY5ohz83SyEmSu1GyKWMv60= zetGZcFixNTHR7h8G3SymgRzNUIyEH7AJiXvbT5LfoUtaoR47bt59OQlvg7Hu3GP+yLf9q3zS8E=

--- containers/dkfz_full/etc/profile.d/odcf-env.sh
+++
@@ -1,6 +0,0 @@
-#
-# Copyright (c) 2023 German Cancer Research Center (DKFZ).
-#
-# Distributed under the MIT License (license terms are at https://github.com/TheRoddyWMS/AlignmentAndQCWorkflows).
-#
-export PATH=${PATH}:/software/bin
